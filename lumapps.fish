0ef01f6d0770dc982ada14fcab8907bab9d4eb61
Thibaut Vuillemin <thibaut@lumapps.com>
2018-07-18 17:17:49 +0200

    lumapps: update the backend alias

--- lumapps.fish
+++ lumapps.fish
@@ -8,4 +8,4 @@ set -x PATH $APPENGINE $N_PREFIX/bin $PATH $HOME/black-env/bin/
 
 # Aliases
 alias frontend='npm start'
-alias backend='rlwrap dev_appserver.py --port 8888 --host=localhost --datastore_path=~/LumApps/datastore --search_indexes_path=~/LumApps/search_index --blobstore_path=~/LumApps/blobstore app/app.yaml app/process.yaml app/frontprocess.yaml --enable_console --enable_sendmail=True --dev_appserver_log_level=warning --threadsafe_override=false --max_module_instance=1'
+alias backend='rlwrap dev_appserver.py --port 8888 --host=localhost --datastore_path=~/LumApps/datastore --search_indexes_path=~/LumApps/search_index --blobstore_path=~/LumApps/blobstore app/app.yaml app/process.yaml app/frontprocess.yaml --support_datastore_emulator=True --enable_console --enable_sendmail=False --dev_appserver_log_level=warning --threadsafe_override=false --max_module_instance=1'
