c46718ca86f9809a772b8e5d64378fbdda96523d
Thibaut Vuillemin <thibaut@lumapps.com>
2018-08-02 12:14:53 +0200

    elasticsearch alias for work

--- lumapps.fish
+++ lumapps.fish
@@ -7,3 +7,4 @@ set -x PATH $PATH $APPENGINE $N_PREFIX/bin
 # Aliases
 alias frontend='npm start'
 alias backend='rlwrap dev_appserver.py --port 8888 --host=localhost --datastore_path=~/LumApps/datastore --search_indexes_path=~/LumApps/search_index --blobstore_path=~/LumApps/blobstore app/app.yaml app/process.yaml app/frontprocess.yaml --support_datastore_emulator=True --enable_console --enable_sendmail=False --dev_appserver_log_level=warning --threadsafe_override=false --max_module_instance=1'
+alias elastic='docker run --interactive --tty --publish 9200:9200 --volume ~/Elastic/data/:/data:rw --rm --name elasticsearch lumapps/elasticsearch'
