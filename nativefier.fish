ab9afa109a07ce598b7f6271e0b38725ec51d064
Thibaut Vuillemin <thibaut@lumapps.com>
2021-02-04 22:29:53 +0100

    fix(nativefier): fix the Slack URL

--- nativefier.fish
+++ nativefier.fish
@@ -5,7 +5,7 @@ function install_nativefied_apps
     nativefier "https://github.com/" --internal-urls ".*github\.com" --name Github
     nativefier "https://lumapps.atlassian.net/secure/Dashboard.jspa?selectPageId=10112" --internal-urls ".*atlassian\.net" --name Jira
     nativefier "https://www.notion.so/6229cf9baeb4436cab1313f074f34af5?v=1f81e5751ecc401bb5155499fed7c169" --internal-urls ".*notion\.so" --name Notion
-    nativefier "https://lumapps.slack.com/archives/C01DVKNDBRV" --internal-urls ".*slack.com" --name Slack
+    nativefier "https://app.slack.com/client/T1BAY6J93/GDVJ4GJ4T" --internal-urls ".*slack.com" --name Slack
     nativefier "https://we.lumapps.com/we/" --internal-urls ".*lumapps\.com|.*\accounts.google.com" --name We
     nativefier "https://web.whatsapp.com/" --internal-urls ".*whatsapp\.com" --name WhatsApp
 end
