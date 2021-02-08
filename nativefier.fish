295a002034a193e1cb1d7535df58e86889e47eee
Thibaut Vuillemin <thibaut@lumapps.com>
2021-02-08 23:03:42 +0100

    feat(nativefier): add Roll20

--- nativefier.fish
+++ nativefier.fish
@@ -5,6 +5,7 @@ function install_nativefied_apps
     nativefier "https://github.com/" --internal-urls ".*github\.com" --name Github
     nativefier "https://lumapps.atlassian.net/secure/Dashboard.jspa?selectPageId=10112" --internal-urls ".*atlassian\.net" --name Jira
     nativefier "https://www.notion.so/6229cf9baeb4436cab1313f074f34af5?v=1f81e5751ecc401bb5155499fed7c169" --internal-urls ".*notion\.so" --name Notion
+    nativefier "https://app.roll20.net/editor/" --internal-urls ".*roll20.net" --name Roll20
     nativefier "https://app.slack.com/client/T1BAY6J93/GDVJ4GJ4T" --internal-urls ".*slack.com" --name Slack
     nativefier "https://we.lumapps.com/we/" --internal-urls ".*lumapps\.com|.*\accounts.google.com" --name We
     nativefier "https://web.whatsapp.com/" --internal-urls ".*whatsapp\.com" --name WhatsApp
