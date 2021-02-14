855e2e55a86492f68e0530644505bf77bb339d3f
Thibaut Vuillemin <thibaut@lumapps.com>
2021-02-14 21:43:40 +0100

    feat(nativefier): add Miro and Drive

--- nativefier.fish
+++ nativefier.fish
@@ -2,8 +2,10 @@ function install_nativefied_apps
     mkdir -p ~/nativefied_apps
     cd ~/nativefied_apps
     nativefier "https://discord.com/channels/694182858767597628" --internal-urls ".*discord\.com" --name Discord
+    nativefier "https://drive.google.com/drive/my-drive" --internal-urls ".*google\.com" --name Drive
     nativefier "https://github.com/" --internal-urls ".*github\.com" --name Github
     nativefier "https://lumapps.atlassian.net/secure/Dashboard.jspa?selectPageId=10112" --internal-urls ".*atlassian\.net" --name Jira
+    nativefier "https://miro.com/app/dashboard/" --internal-urls ".*miro\.com" --name Miro
     nativefier "https://www.notion.so/6229cf9baeb4436cab1313f074f34af5?v=1f81e5751ecc401bb5155499fed7c169" --internal-urls ".*notion\.so" --name Notion
     nativefier "https://app.roll20.net/editor/" --internal-urls ".*roll20.net" --name Roll20
     nativefier "https://app.slack.com/client/T1BAY6J93/GDVJ4GJ4T" --internal-urls ".*slack.com" --name Slack
