4788c7f00eeb8c58f13932119d611ba0e195301d
Thibaut Vuillemin <contact@thibautvuillemin.com>
2023-05-12 16:22:41 +0200

    add aliases for lumapps tools

--- lumapps.fish
+++ lumapps.fish
@@ -1,2 +1,4 @@
 alias k=kubectl
 alias kgp="kubectl get pods"
+alias uuid="~/Github/lumapps/core/local/scripts/id_to_uuid.py"
+alias sdv="~/Github/lumapps/core/local/scripts/show_deployed_versions.py"
