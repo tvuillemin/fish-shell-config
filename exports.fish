d296a49b5a511de40ae8d3409483851e4c16d027
Thibaut Vuillemin <contact@thibautvuillemin.com>
2023-09-15 10:32:52 +0200

    fix gcloud

--- exports.fish
+++ exports.fish
@@ -1,2 +1,6 @@
 set -x UID (id -u $USER)
 set -x EDITOR vim
+
+# set -U $fish_user_paths /usr/local/opt/make/libexec/gnubin /usr/local/opt/gnu-sed/libexec/gnubin /usr/local/opt/gnu-getopt/bin /usr/local/opt/openssl@1.1/bin
+fish_add_path /usr/local/opt/gnu-sed/libexec/gnubin
+set -x LIBRARY_PATH /usr/local/opt/openssl/lib/
--- lumapps.fish
+++ lumapps.fish
@@ -2,3 +2,7 @@ alias k=kubectl
 alias kgp="kubectl get pods"
 alias uuid="~/Github/lumapps/core/local/scripts/id_to_uuid.py"
 alias sdv="~/Github/lumapps/core/local/scripts/show_deployed_versions.py"
+
+source /usr/local/opt/asdf/libexec/asdf.fish
+set -x CLOUDSDK_PYTHON /usr/local/bin/python3
+source "$(brew --prefix)/share/google-cloud-sdk/path.fish.inc"
